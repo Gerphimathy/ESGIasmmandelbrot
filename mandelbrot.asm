3d05ffe61b75425a58e0cce6b657aa9bc6899312
Math <philipponmathis@gmail.com>
2021-11-22 21:19:58 +0100

    Copie (en partie) de l'algo donné en cours, besoin de débug

--- mandelbrot.asm
+++ mandelbrot.asm
@@ -44,12 +44,20 @@ height:        	resd	1
 window:		resq	1
 gc:		resq	1
 
+maxIter: resb 1
+sqr: resq 2
+
 section .data
 
 event:		times	24 dq 0
 
-x:	dd	0
-y:	dd	0
+; Nombre Complexe Z
+zre: dd 0
+zim: dd 0
+
+; Nombre Complexe C
+cre: dd 0
+cim: dd 0
 
 section .text
 	
@@ -77,8 +85,11 @@ mov rdi,qword[display_name]
 mov rsi,rbx
 mov rdx,10
 mov rcx,10
+
+; TODO: Remove this and scan for the values on program start instead
 mov r8,400	; largeur
 mov r9,400	; hauteur
+
 push 0xFFFFFF	; background  0xRRGGBB
 push 0x00FF00
 push 1
@@ -121,6 +132,135 @@ jmp boucle
 ;#		DEBUT DE LA ZONE DE DESSIN		 #
 ;#########################################
 dessin:
+
+
+
+mov r14d, 0 ; y = 0
+mov r15d, 0 ; x = 0
+
+; TODO: Remove this and scan for the value on program start instead
+mov byte[maxIter], 50
+
+forEachLine: ;for (x = 0; x < width; x++)
+    forEachColumn: ;for (y = 0; y < height; y++)
+
+    mov dword[cre], r15d
+    mov dword[cim], r14d
+    ; c = x (r15) + y(r14)i
+
+    mov dword[zre], 0
+    mov dword[zim], 0
+    ; z = 0 + 0i
+
+    mov r13b, 0
+    ; iteration = 0
+
+        boucleDessin: ;do
+        mov rcx, 0
+        ;push dword[zre] ; temp = zre
+        ; Donne Erreur d'assemblage, utilisation de r10d
+        mov r10d, dword[zre]
+
+        ; zre = zre*zre - zim*zim + cre
+
+        mov rbx, sqr ; rbx --> sqr[0]
+        mov eax, dword[zre]
+        mul dword[zre]
+        mov [rbx], eax
+        mov [rbx+DWORD], edx
+        ; sqr[0] = zre²
+
+        mov rbx, sqr+QWORD ; rbx --> sqr[1]
+        mov eax, dword[zim]
+        mul dword[zim]
+        mov [rbx], eax
+        mov [rbx+DWORD], edx
+        ; sqr[1] = zim²
+
+        ; TODO: Enforcer une taille limite pour éviter un dépassement de capacité
+        mov rax, qword[sqr] ; zre²
+        sub rax, qword[sqr+QWORD] ; - zim²
+        add rax, r15 ; + cre
+        mov dword[zre], eax
+        mov rax, 0
+        ; zre = zre² - zim² + cre
+
+
+        ; zim = 2*zim*temp + cim
+
+
+        ;pop ebx ; ebx = temp
+        ; Donne Erreur d'assemblage, utilisation de r10d
+
+        mov ebx, r10d
+        shl ebx, 1 ; ebx *= 2
+
+        mov eax, dword[zim]
+        mul ebx
+        mov ecx, eax ; ecx =  ebx*zim
+        ; ecx = 2*temp*zim
+        ; TODO: Enforcer une taille limite pour éviter un dépassement de capacité
+
+        add ecx, dword[cim]
+        mov dword[zim], ecx
+        mov ecx, 0
+        ; zim = 2*zim*temp + cim
+
+
+        inc r13 ; i++
+
+        ; rax = zre*zre + zim*zim
+
+        mov rbx, sqr ; rbx --> sqr[0]
+        mov eax, dword[zre]
+        mul dword[zre]
+        mov [rbx], eax
+        mov [rbx+DWORD], edx
+        ; sqr[0] = zre²
+
+        mov rbx, sqr+QWORD ; rbx --> sqr[1]
+        mov eax, dword[zim]
+        mul dword[zim]
+        mov [rbx], eax
+        mov [rbx+DWORD], edx
+        ; sqr[1] = zim²
+
+        mov rcx, qword[sqr] ; rcx = zre²
+        add rcx, qword[sqr+QWORD] ; rcx = zre² + zim²
+
+        ; rcx = zre*zre + zim*zim
+
+        ; while zre*zre + zim*zim < 4
+        cmp rcx, 4
+        jb boucleDessin
+        ; and i < maxIter
+        cmp r13b, byte[maxIter]
+        jb boucleDessin
+
+    cmp r13, maxIter ;if i = maxIter
+    jne finForEach
+    ; Draw Point
+        ; Point Color
+        mov rdi,qword[display_name]
+        mov rsi,qword[gc]
+        mov edx,0x000000	; Black
+        call XSetForeground
+
+        ;;;dessin du point
+        mov rdi,qword[display_name]
+        mov rsi,qword[window]
+        mov rdx,qword[gc]
+        mov ecx,r15d	; coordonnée source en x
+        mov r8d,r14d	; coordonnée source en y
+        call XDrawPoint
+
+    finForEach:
+    inc r14d
+    cmp r14d, dword[height]
+    jb forEachColumn
+inc r15d
+cmp r15d, dword[width]
+jb forEachLine
 
 ;#########################################
 ;#		EXEMPLE UTILISATION XDRAWPOINT   #
